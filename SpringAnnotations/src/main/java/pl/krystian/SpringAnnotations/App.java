e9d58fdd2990a68d0d33f5ac56c4e26d18b75720
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-05-07 12:38:01 +0200

    Project initialized

---
+++ SpringAnnotations/src/main/java/pl/krystian/SpringAnnotations/App.java
@@ -0,0 +1,13 @@
+package pl.krystian.SpringAnnotations;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
